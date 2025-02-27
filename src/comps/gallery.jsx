5893437e8ecc8735ea1846075900b9de327b0341
Venkat Koushik <144405842+vkoushik15@users.noreply.github.com>
2025-02-27 09:46:11 +0530

    Update gallery.jsx

--- src/comps/gallery.jsx
+++ src/comps/gallery.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import '../styling/gallery.css'; // External CSS file
+import '../styling/gallery.css'; 
 
-// Import your images (replace with your actual image paths)
+
 import Image1 from '../clubimages/1.jpg';
 import Image2 from '../clubimages/2.jpg';
 import Image3 from '../clubimages/3.jpg';
@@ -11,7 +11,6 @@ import Image6 from '../clubimages/6.jpg';
 import Image7 from '../clubimages/7.jpg';
 import Image8 from '../clubimages/8.jpg';
 
-// Import another set of images for the second gallery
 import Image9 from '../clubimages/9.jpg';
 import Image10 from '../clubimages/10.jpg';
 import Image11 from '../clubimages/11.jpg';
@@ -22,20 +21,20 @@ import Image15 from '../clubimages/15.jpg';
 
 
 const Gallery = () => {
-  const imagesLeft = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8]; // First set of images
-  const imagesRight = [Image9, Image10, Image11, Image12, Image13, Image14, Image15]; // Second set of images
+  const imagesLeft = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8];
+  const imagesRight = [Image9, Image10, Image11, Image12, Image13, Image14, Image15]; 
 
   return (
     <div className="gallery-container">
-      {/* First gallery (scrolls to the left) */}
+      
       <div className="gallery gallery-left">
-        {/* First set of images */}
+       
         {imagesLeft.map((image, index) => (
           <div key={index} className="gallery-item">
             <img src={image} alt={`Gallery Image ${index + 1}`} />
           </div>
         ))}
-        {/* Duplicate set of images for seamless looping */}
+        
         {imagesLeft.map((image, index) => (
           <div key={`dup-left-${index}`} className="gallery-item">
             <img src={image} alt={`Gallery Image ${index + 1}`} />
@@ -43,15 +42,15 @@ const Gallery = () => {
         ))}
       </div>
 
-      {/* Second gallery (scrolls to the right) */}
+     
       <div className="gallery gallery-right">
-        {/* First set of images */}
+       
         {imagesRight.map((image, index) => (
           <div key={index} className="gallery-item">
             <img src={image} alt={`Gallery Image ${index + 9}`} />
           </div>
         ))}
-        {/* Duplicate set of images for seamless looping */}
+       
         {imagesRight.map((image, index) => (
           <div key={`dup-right-${index}`} className="gallery-item">
             <img src={image} alt={`Gallery Image ${index + 9}`} />
